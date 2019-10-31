c150341c97725c018dfc1fec12e95f53f895b545
Max Base <Max@Asrez.com>
2019-10-31 13:43:21 +0330

    Update google search...

--- google-search.py
+++ google-search.py
@@ -1,11 +1,14 @@
+# Max Base
+# https://github.com/BaseMax
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
+browser = webdriver.Chrome()
 browser.get('https://www.google.com/')
 print(browser.title)
 
-search = browser.find_element_by_name('input')
+# document.querySelector("input.gLFyf.gsfi")
+search = browser.find_element_by_class_name('gLFyf')
 search.send_keys('BaseMax' + Keys.RETURN)
 
 browser.quit()
