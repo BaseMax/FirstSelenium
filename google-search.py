60962550a1196997251a07d1c29257c16e7c0fa7
Poojita9185 <poojita9185@gmail.com>
2023-10-04 22:22:20 +0530

    removed comment

--- google-search.py
+++ google-search.py
@@ -5,8 +5,6 @@ from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
 browser.get('https://www.google.com/')
-
-# document.querySelector("input.gLFyf.gsfi")
 search = browser.find_element_by_class_name('gLFyf')
 search.send_keys('BaseMax' + Keys.RETURN)
 print(browser.title)
